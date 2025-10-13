4cac09e885a3535658dc97b3439853ed66a92b6e
Вячеслав Туйков <tuykov.ve@gmail.com>
2025-10-13 10:36:34 +0300

    homework2 complete

---
+++ .idea/Homeworks/hw-lesson2/homework-lesson2.py
@@ -0,0 +1,6 @@
+from dateutil import parser
+
+dates1 = {}
+
+dates = {"Wednesday, October 2, 2002", "Friday, 11.10.13", "Thursday, 18 August 1977"}
+for data in dates: dataobj = print (parser.parse(data))
